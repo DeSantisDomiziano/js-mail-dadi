b96fec834b0eaa6eac1a5dc60a41eb1dd4ba7071
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-13 16:05:26 +0100

    generator random nuber for pc

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,10 @@ btnEl.addEventListener('click', function() {
     }
     
 });
+
+
+/* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+console.log(pcNumber)
