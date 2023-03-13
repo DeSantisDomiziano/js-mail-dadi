036d67bc454bb82c1f146a00f73dce8c0e8fb89a
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-13 14:45:15 +0100

    add list on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,25 @@
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 Mail Bonus
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina */
+
+//lista invitati
+
+const class91 = [
+    'adam.ben@gmail.com',
+    'alessia.bertinato@gmail.com',
+    'alexandro.fioretti@gmail.com',
+    'andrea.ferrari@gmail.com',
+    'blasi.mario@gmail.com',
+    'carmelo.leone@gmail.com',
+    'damiano.cosolari@gmail.com',
+    'daniel.oddo@gmail.com',
+    'daniel.morello@gmail.com',
+    'domiziano.desantis@gmail.com',
+    'donato.riccio@gmail.com',
+    'edoardo.rizzo@gmail.com',
+    'enrico.gian@gmail.com',
+    'fabio.delli@gmail.com',
+    'fabio.pacifici@gmail.com',
+    'fabrizio.guariglia@gmail.com',
+//siete troppi non metto tutti :)
+]
